cac9a5e3efeb75309025d3c08318b45c2ed14507
INFJakZda <zdanowski78@gmail.com>
2019-03-09 11:46:03 +0100

    Prepare MST function and draft of 'how to..'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import numpy as numpy
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import euclidean_distances
+from scipy.sparse.csgraph import minimum_spanning_tree
 
 def loadInstance(fileName):
     with open(fileName) as fp:
@@ -10,6 +11,9 @@ def loadInstance(fileName):
 
 def prepareMatrix(samples):
     return euclidean_distances(samples, samples)
+
+def calculateLengthMST(matrix):
+    return np.sum(minimum_spanning_tree(matrix))
 
 def draw(x, y):
     plt.scatter(x, y)
@@ -29,9 +33,24 @@ if __name__ == '__main__':
     # III calculate matrix with euclidean distances
     matrix = prepareMatrix(samples)
 
-    # IV 
+    # IV Prepare function to calculate MST
+    # This function takes matrix and return length of minimum spanning tree
+    #length = calculateLengthMST(matrix)
 
+    # V TODO Greedy
+    #   - Divide matrix on 10 groups
+    #   - In every group is one element
+    #   - Add to every ggroup element which is closest to this ele
 
+    # VI TODO Regret
+    #   - Divide matrix on 10 groups
+    #   - In every group is one element
+    #   - Add to every ggroup element with calculated regret
+
+    # VII TODO prepare tests
+    #   - 100 tests for 2 methods
+    #   - save best solution and its image with colored groups
+    #   - calculate min, max, mean
 
 
 
