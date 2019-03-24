673c275c62838802b8dbe267a564049385560533
unknown <blatos10@gmail.com>
2019-03-24 20:49:34 +0100

    Add count_result and delta

--- sprawko2.py
+++ sprawko2.py
@@ -2,6 +2,26 @@ from helpers import loadInstance, prepareMatrix, randomGroups
 from algorithms import greedMethod
 from drawing import drawRegret
 
+def count_result(groups, matrix):
+    sum_all = 0
+    count_pairs = 0
+    for group in groups:
+        for i in range(len(group)):
+            for j in range(i+1, len(group)):
+                sum_all += matrix[group[i]][group[j]]
+        count_pairs += sum(range(len(group)))
+    return sum_all / count_pairs, sum_all, count_pairs
+
+def delta(old_groups, element, matrix): #element - [id, stara grupa, nowa grupa]
+    sum_amplitude = 0
+    pairs_amplitude = 0
+    for edge in old_groups[element[1]]:
+        sum_amplitude -= matrix[id][edge]
+    for edge in old_groups[element[2]]:
+        sum_amplitude += matrix[id][edge]
+    pairs_amplitude -= sum(range(len(old_groups[element[1]]))) + sum(range(len(old_groups[element[2]])))
+    pairs_amplitude += sum(range(len(old_groups[element[1]] - 1))) + sum(range(len(old_groups[element[2]] + 1)))
+    return sum_amplitude, pairs_amplitude
 
 if __name__ == '__main__':
     no_groups = 20
@@ -46,3 +66,6 @@ if __name__ == '__main__':
     #   - 100 tests for 4 methods
     #   - save best solution and its image with colored groups
     #   - calculate min, max, mean
+
+	print(count_result(random_groups, matrix))
+    print(count_result(greed_groups, matrix))
