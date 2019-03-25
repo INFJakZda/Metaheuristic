049a81b88d4c7df8ef499328183f3a8856c97217
unknown <blatos10@gmail.com>
2019-03-25 13:52:17 +0100

    Add steepest algorithm

--- sprawko2.py
+++ sprawko2.py
@@ -25,6 +25,46 @@ def delta(old_groups, element, matrix, old_sum_all, old_count_pairs): #element -
     
     return (old_sum_all + sum_amplitude) / (old_count_pairs + pairs_amplitude)
 
+def checkGroup(ele, groups):
+    for idx, group in enumerate(groups):
+        if ele in group:
+            return idx
+
+def steepest(groups, matrix):
+    for i in range(1000):
+        result, sum_all, count_pairs = count_result(groups, matrix)
+        minimal = result
+        neighbours_all_groups = []
+        for group in groups:
+            neighbours_all_groups.append(get_neighbours_from_other_groups(group, matrix, 20+(i+2)))
+            
+        for j in range(len(neighbours_all_groups)):
+            for idx, edge in enumerate(neighbours_all_groups[j]):
+                for value in neighbours_all_groups[j][edge]:
+                    group_idx = checkGroup(value[0], groups)
+                    current = delta(groups, [edge, j, group_idx], matrix, sum_all, count_pairs)
+                    if(current < minimal):
+                        minimal = current
+                        element = [edge, group_idx, j]
+    
+        print(minimal)
+        if (element == [0,0,0]):
+            break
+        else:
+            groups[element[2]].remove(element[0])
+            groups[element[1]].append(element[0])
+        element = [0,0,0]
+        #result = minimal
+    return groups
+    
+def get_neighbours_from_other_groups(group, matrix, niegh_dist):    #returns dict with {ele: [(element_index, distance), ...]} for every ele from this group
+    neighbours = dict((ele, []) for ele in group)
+    for idx in group:
+        neighbours[idx] = matrix[idx]
+        neighbours[idx] = [(i, x) for i, x in enumerate(neighbours[idx]) if i not in group and x < niegh_dist]
+        neighbours[idx].sort(key=lambda tup: tup[1])
+        #neighbours[idx].pop(0)
+    return neighbours
 
 if __name__ == '__main__':
     no_groups = 20
@@ -70,7 +110,12 @@ if __name__ == '__main__':
     #   - save best solution and its image with colored groups
     #   - calculate min, max, mean
 
-	print(count_result(random_groups, matrix))
+    #print(count_result(random_groups, matrix))
     result, sum_all, count_pairs = count_result(greed_groups, matrix)
     print(result, sum_all, count_pairs)
     print(delta(greed_groups,[greed_groups[2][0], 2, 5], matrix, sum_all, count_pairs))
+    
+    #print(get_neighbours_from_other_groups(greed_groups[0], matrix, 30))
+    
+    steepest_groups = steepest(greed_groups, matrix)
+    drawRegret(x_samples, y_samples, steepest_groups)
